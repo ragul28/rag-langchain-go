7d74bcbfdc57f08392f67eb3eedc1f290b670bbe
ragul28 <ragulh28@gmail.com>
2024-10-27 23:00:15 +0530

    Initial code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RAG LangChain
+
+This simple RAG (Retrieval Augmented Generation) golang based example using google gemini.
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+const generativeModelName = "gemini-1.5-flash"
+const embeddingModelName = "text-embedding-004"
+
+func main() {
+	fmt.Println("Running RAG model", generativeModelName)
+}
