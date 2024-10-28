64f4b6d08dc1476c6defb25fd5471b81dc748f2b
ragul28 <ragulh28@gmail.com>
2024-10-28 22:58:13 +0530

    Add langchaingo with geminiClient

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/tmc/langchaingo/embeddings"
+	"github.com/tmc/langchaingo/llms/googleai"
+)
 
 const generativeModelName = "gemini-1.5-flash"
 const embeddingModelName = "text-embedding-004"
 
 func main() {
 	fmt.Println("Running RAG model", generativeModelName)
+
+	ctx := context.Background()
+	apiKey := os.Getenv("GEMINI_API_KEY")
+	geminiClient, err := googleai.New(ctx,
+		googleai.WithAPIKey(apiKey),
+		googleai.WithDefaultEmbeddingModel(embeddingModelName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	emb, err := embeddings.NewEmbedder(geminiClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(emb)
 }
